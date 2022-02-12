f1601c3b357f7f3d5499a3a05f65910c59e977f5
Amy Shafer <amy.u.shafer@gmail.com>
2022-02-11 23:19:42 -0500

    dropdown shapes are functional

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,36 @@
 const projects = $(".dropdown-items")
-const squareForLiveLink = $("#work-square");
-const triangleForRepoLink = $("#work-triangle");
-const circleForYear = $("#work-circle");
 const yellowTriangle = $("#yellow-triangle");
 const blueTriangle = $("#blue-triangle");
 const pinkTriangle = $("#pink-triangle");
+const squareForLiveLink = $("#work-square > a"); 
+const circleForYear = $("#work-circle > a");
+const triangleForRepoLink = $("#work-triangle > a");
 
 const projectsArray = [
     {
-        title: "Computer Science Icons Quiz",
-        date: "December 2021",
+        title: "CS Icons Quiz",
+        date: "Dec. 2021",
         deployedLink: "https://amyshafer.github.io/CS-Icons-Quiz/",
         repoLink: "https://github.com/AmyShafer/CS-Icons-Quiz",
         gif: "./assets/images/CS-Icons-Quiz.gif"
     },
     {
-        title: "It's Raining Men...Or Is It?",
-        date: "February 2022",
+        title: "Weather App",
+        date: "Feb. 2022",
         deployedLink: "https://amyshafer.github.io/Its-Raining-Again-Or-Is-It",
         repoLink: "https://github.com/AmyShafer/Its-Raining-Again-Or-Is-It",
         gif: "./assets/images/weatherGirlsApp.gif"
     },
     {
         title: "Mad Archer's Cookbook",
-        date: "January 2022",
+        date: "Jan. 2022",
         deployedLink: "https://amyshafer.github.io/Mad-Archers-Cookbook",
         repoLink: "https://github.com/AmyShafer/Mad-Archers-Cookbook",
         gif: "./assets/images/madArchersCookbook.gif"
     },
     {
-        title: "Scheduler for the Wicked Quiz",
-        date: "January 2022",
+        title: "Scheduler",
+        date: "Jan. 2022",
         deployedLink: "https://amyshafer.github.io/Scheduler-for-the-Wicked ",
         repoLink: "https://github.com/AmyShafer/Scheduler-for-the-Wicked ",
         gif: "./assets/images/scheduler666.gif"
@@ -38,45 +38,39 @@ const projectsArray = [
 ]
 
 /* Function allows user to select a project to look at */
-function showProject() {
-  // hide all the triangles     
-  $("yellowTriangle").attr("display", "none");
-  $("blueTriangle").attr("display", "none");
-  $("pinkTriangle").attr("display", "none");
-  // show the gif of the selected project
+function showProject(event) {
+  event.preventDefault();
+  // hide all the triangles  
+  const selectedProject = $(event.currentTarget).text();
+  const index = $(event.currentTarget).find('a:first').attr("data-set");
+  yellowTriangle.hide();
+  blueTriangle.hide();
+  pinkTriangle.hide();
 
   // change the text of "Live Link" to the title of the selected project and a link to the deployed project
-
+  squareForLiveLink.attr("href", projectsArray[index].deployedLink);
+  squareForLiveLink.text(projectsArray[index].title)
   // change the date to the month and year the project was most recently published to GitHub
-
+  circleForYear.attr("href", projectsArray[index].deployedLink);
+  circleForYear.text(projectsArray[index].date)
   // change the Repo link so it is a link to the Repo
+  triangleForRepoLink.attr("href", projectsArray[index].repoLink);
+  triangleForRepoLink.text("Repo Link")
 }  
 
-/* jQuery event listener - listens for the project being selected in the dropdown menu */
-// $(function() {
-//   $(".dropdown-menu").on("click", function() {
-//     let selectedProject = $(".dropdown-menu option:selected").text();
-//     console.log(selectedProject);
-//    }) 
-// })
+/* jQuery event listener - listens for the dropdown menu to be clicked */
+$('#dropdownMenuButton1').on("click", function () {
+  const dropdownBtn = $(this).siblings("ul");
+  if (dropdownBtn.attr("class").includes("hidden")) {
+    dropdownBtn.removeClass("hidden");
+  } else {
+    dropdownBtn.addClass("hidden");
+  } 
+})
 
-  $('#dropdownMenuButton1').on("click", function () {
-    const dropdownBtn = $(this).siblings("ul");
-    if (dropdownBtn.attr("class").includes("hidden")) {
-      dropdownBtn.removeClass("hidden");
-    } else {
-      dropdownBtn.addClass("hidden");
-    } 
+/* jQuery event listener - listens for the save buttons being clicked */
+$('li').each(function() {
+  $(this).click(function(event) {
+    showProject(event);
   })
-
-  /*
-  $( "select" )
-  .change(function () {
-    var str = "";
-    $( "select option:selected" ).each(function() {
-      str += $( this ).text() + " ";
-    });
-    $( "div" ).text( str );
-  })
-  .change();
-  */
+})
